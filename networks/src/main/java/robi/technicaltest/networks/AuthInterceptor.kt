da19918fdc1ec0a0f39c1eb243ab2fa06ec05218
Robi A. Saputra <robby.adhy@gmail.com>
2025-02-07 20:00:27 +0700

    initial Commit

--- networks/src/main/java/robi/technicaltest/networks/AuthInterceptor.kt
+++ networks/src/main/java/robi/technicaltest/networks/AuthInterceptor.kt
@@ -6,7 +6,6 @@ import okhttp3.Response
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer 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")
             .addHeader("Accept", "application/vnd.github.v3+json")
             .build()
         return chain.proceed(request)
